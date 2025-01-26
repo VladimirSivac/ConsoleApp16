7f70e20323a05638bbc4ca06f62ef46a4483c213
VladimirSivac <morndes@gmail.com>
2025-01-26 19:30:40 +0200

    task-4 done

--- Program.cs
+++ Program.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp16
 {
+    /// <summary>
+    /// Вывести на экран наклонную линию (\) из 8 символов.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Основной метод
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-           
+            Func(8);
+        }
+        /// <summary>
+        /// Дополнительный метод для вывода наклонной линии
+        /// </summary>
+        /// <param name="length">длина линии</param>
+        static void Func(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("#");
+            }
         }
     }
 }
