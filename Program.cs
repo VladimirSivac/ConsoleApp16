ee32154c13361bdf82c8973d843c7ce1a87230c1
VladimirSivac <morndes@gmail.com>
2025-01-26 19:26:26 +0200

    task-3 done

--- Program.cs
+++ Program.cs
@@ -6,11 +6,41 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp16
 {
+    /// <summary>
+    /// Вывести на экран прямоугольник вида □, обрамленный символами (например, 16 на 16) и пустой внутри.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Главный метод
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-           
+           Func(16, 16);
+        }
+        /// <summary>
+        /// Дополнительный метод для вывода прямоугольника
+        /// </summary>
+        /// <param name="width">Ширина прямоугольника</param>
+        /// <param name="height">Высота прямоугольника</param>
+        static void Func(int width, int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
